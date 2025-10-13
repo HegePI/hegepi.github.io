ed7de4a53a88d9258d850c482b3d5c3e04ef42b1
Heikki Pulli <heikki.pulli@helsinki.fi>
2025-10-13 11:34:04 +0300

    fix: correct formatting and wording in portfolio introduction

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,13 +9,16 @@
 		<section>
 			<h1>Welcome to my portfolio!</h1>
 
-			<p>Here are collected all the relevanat stuff which tells the story of me as a developer.</p>
+			<p>
+				Here I have are collected all the relevanat stuff which tells the story of me as a
+				developer.
+			</p>
 		</section>
 		<section>
 			<h2><a href={resolve('/projects')}>Projects</a></h2>
 			<p>
-				In the projects page are listed my personal projects that I have started. Not all projects
-				are finished.
+				In the projects page I have listed my personal projects that I have started. Not all
+				projects are finished.
 			</p>
 		</section>
 		<section>
