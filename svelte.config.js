ef73d5a7cc2a96cf93ff47a59c0726c8a9dadc92
Heikki Pulli <heikki.pulli@helsinki.fi>
2025-11-28 13:37:14 +0200

    removed paths

--- svelte.config.js
+++ svelte.config.js
@@ -12,10 +12,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
 			fallback: '200.html'
-		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH || ''
-		}
+		})
 	}
 };
 
