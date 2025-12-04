b26eb3ee73d4386e4e580ae4786fd05e5d4a9fdc
Heikki Pulli <heikki.pulli@helsinki.fi>
2025-12-04 14:02:37 +0200

    - playwright config updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,12 +4,15 @@ export default defineConfig({
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 4173,
-		reuseExistingServer: true
+		reuseExistingServer: !process.env.CI
 	},
 	testDir: 'e2e',
 	retries: process.env.CI ? 2 : 0,
 	use: {
 		trace: 'on-first-retry'
 	},
-	reporter: 'html'
+	reporter: 'html',
+	workers: process.env.CI ? 1 : undefined,
+	forbidOnly: !!process.env.CI,
+	fullyParallel: true
 });
